09f4d5d23e3053b9494f14b7dcddafdd9175ae1b
nimstill <1679917335@qq.com>
2016-10-29 00:34:26 +0800

    reset

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' } #
+    return '''
+
+<html>
+<head>
+<title>Home Page</title>
+</head>
+<body>
+<h1>Hello, ''' + user['nickname'] + '''</h1>
+</body>
+</html>
+'''
+
