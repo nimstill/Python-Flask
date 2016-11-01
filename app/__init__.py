6192237685e22154ce44e1de07e46833c0da4bb6
nimstill <1679917335@qq.com>
2016-11-01 23:55:41 +0800

    ch4

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,13 @@ db = SQLAchemy(app)
 from app import views, models
 
 
+import os
+from flask.ext.login import LoginManger
+from flask.ext.openid import OpenID
+from config import basedir 
+
+lm = LoginManger()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+
