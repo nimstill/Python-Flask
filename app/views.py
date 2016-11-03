9bf64ee6f4f6442e7e72d0a252dc54a7157f8a77
nimstill <1679917335@qq.com>
2016-11-04 00:26:40 +0800

    ch4

--- app/views.py
+++ app/views.py
@@ -63,4 +63,23 @@ def login():
 # oid.loginhandle 告诉 Flask-OpenID 这是我们的登录视图函数。
 # Flask 中的 g 全局变量是一个在请求生命周期中用来存储和共享数据。
 
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == ""
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == ""
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
