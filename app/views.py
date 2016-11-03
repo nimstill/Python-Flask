6e781ed087b0dae6c28bca7497756715b033f838
nimstill <1679917335@qq.com>
2016-11-04 00:18:59 +0800

    ch4

--- app/views.py
+++ app/views.py
@@ -35,3 +35,32 @@ def login():
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
     
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
+from .forms import LoginForm
+from .models import User
+
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+# oid.loginhandle 告诉 Flask-OpenID 这是我们的登录视图函数。
+# Flask 中的 g 全局变量是一个在请求生命周期中用来存储和共享数据。
+
+
