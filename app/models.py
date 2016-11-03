21898af742c6dcd614befc70b526193d7a0fbf37
nimstill <1679917335@qq.com>
2016-11-03 23:53:54 +0800

    ch4

--- app/models.py
+++ app/models.py
@@ -19,4 +19,27 @@ class Post(db.Model):
         return '<Post %r>' % (self.body)
 
 
-        
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), unique = True)
+    email = db.Column(db.String(120), unique = True)
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)        
