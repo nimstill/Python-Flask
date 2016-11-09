bd09920ca07282800abfcacb7caa50010d646e03
nimstill <1679917335@qq.com>
2016-11-09 23:51:19 +0800

    ch8

--- app/models.py
+++ app/models.py
@@ -55,3 +55,23 @@ class User(db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
 
+
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(120), index = True, unique = True)
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+        secondary = followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'),
+        lazy = 'dynamic')
+
